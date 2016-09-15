ad2db4e439449496ea2ba0e398bd70f991366f25
Thomas Cho <s.thomas.cho@gmail.com>
2016-09-15 15:39:13 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Complete blogging application made with React, Express, Node, Webpack, React-Router
+
+npm install
+npm start
---
+++ index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from 'react-dom'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+import App from './modules/App'
+
+render((
+  <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+  </Router>
+), document.getElementById('app'))
