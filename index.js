01de5384f45697ac8ea2d78cf4e628c351de6001
Thomas Cho <s.thomas.cho@gmail.com>
2016-09-21 21:33:22 -0700

    Add react-bootstrap

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import { render } from 'react-dom'
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './modules/App'
 import About from './modules/About'
-import UserBox from './modules/UserBox'
+import Login from './modules/Login'
 import Home from './modules/Home'
 
 const NoMatch = () => <h2>No match to the route</h2>;
 
 render((
   <Router history={browserHistory}>
-    <Route path="/" component={UserBox} />
+    <Route path="/" component={Login} />
     <Route path="/:userId" component={Home} />
     <Route path="/about" component={About}/>
     <Route path="*" component={NoMatch} />
--- modules/Home.js
+++ modules/Home.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from 'react-bootstrap'
 
 export default React.createClass({
 	render: function() {
 		return (
 			<div>
+				<Button bsStyle="primary">Default button</Button>
 				<h2>{this.props.params.userId}</h2>
 			</div>
 		)
